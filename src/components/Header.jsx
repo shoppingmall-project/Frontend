ec4b5fa217ded12355d89b58555c48030431801e
YoungWonius <59616376+YoungWonius@users.noreply.github.com>
2022-06-09 14:10:46 +0900

    Header 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Header() {
-  const [isLogin, setIsLogin] = useState();
+  const [isLogin, setIsLogin] = useState(false);
+  const [isManager, setIsManager] = useState(false);
 
   useEffect(() => {
     if (sessionStorage.getItem("jwtToken")) setIsLogin(true);
+    axios
+      .get(`http://54.180.53.149:8080/auth/${sessionStorage.getItem("id")}`)
+      .then((res) => {
+        if (res.data.data.role === "M") setIsManager(true);
+      })
+      .catch();
   }, []);
 
   return (
@@ -33,6 +41,22 @@ function Header() {
                 Community
               </Link>
             </li>
+            {isManager === true ? (
+              <>
+                <li>
+                  <Link to={"/manager"} className="navLink">
+                    manager
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/myPage"} className="navLink">
+                    My Page
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <></>
+            )}
             {isLogin ? (
               <>
                 <li>
