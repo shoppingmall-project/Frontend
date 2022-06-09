2892d3b35adfb42e6ac9fcf6010c11fc0b9831e8
YoungWonius <59616376+YoungWonius@users.noreply.github.com>
2022-06-09 12:45:06 +0900

    Board 삭제 기능 추가

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -8,6 +8,24 @@ function BoardPage() {
   const [board, setBoard] = useState(0);
   let { id } = useParams();
   moment.locale("ko");
+
+  const onClickDelete = (e) => {
+    e.preventDefault();
+    axios
+      .delete(`http://54.180.53.149:8080/board/${board.boardId}`, {
+        headers: { "X-AUTH-TOKEN": sessionStorage.getItem("jwtToken") },
+      })
+      .then((res) => {
+        if (res.data.result === "FAIL") {
+          alert("게시글 삭제에 실패하였습니다.");
+        } else {
+          alert("게시글 삭제 성공");
+          document.location.href = "/";
+        }
+      })
+      .catch();
+  };
+
   useEffect(() => {
     axios
       .get(`http://54.180.53.149:8080/board/${id}`)
@@ -20,6 +38,10 @@ function BoardPage() {
       <div className={styles.container}>writer:{board.writer}</div>
       <div className={styles.container}>content:{board.content}</div>
       <div className={styles.container}>views:{board.views}</div>
+      <div>
+        <button>Modify</button>
+        <button onClick={onClickDelete}>Delete</button>
+      </div>
     </div>
   );
 }
