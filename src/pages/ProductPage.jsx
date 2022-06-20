3f2c112d243aaf9d9c6f01ef44bfd1e219dc3a18
YoungWonius <59616376+YoungWonius@users.noreply.github.com>
2022-06-20 13:41:37 +0900

    ProductPage 상품사진 수정

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -6,9 +6,21 @@ import defaultImg from "../imgs/NoImg.png";
 import { useParams } from "react-router";
 
 function ProductPage() {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState({
+    id: 0,
+    category: "",
+    name: "",
+    price: 0,
+    stock: 0,
+    description: "",
+    brand: "",
+    country: "",
+    imageUrl: "",
+    seller: "",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [url, setUrl] = useState("");
   const { productid } = useParams();
-
   const handleImgError = (e) => {
     e.target.src = defaultImg;
   };
@@ -19,25 +31,31 @@ function ProductPage() {
       .then((res) => {
         setProduct(res.data.data);
       })
+      .then(() => {
+        setUrl(
+          "https://wineshoppingmall.s3.ap-northeast-2.amazonaws.com/" +
+            product.imageUrl.substring(25)
+        );
+        setIsLoading(false);
+      })
       .catch();
-  }, []);
+  }, [url]);
 
   return (
-    <div className="page">
-      <img
-        src={require(product.imageUrl)}
-        alt="사진"
-        onError={handleImgError}
-      />
-      <div>제품번호:{product.id}</div>
-      <div>카테고리:{product.category}</div>
-      <div>제품명:{product.name}</div>
-      <div>가격:{product.price}</div>
-      <div>브랜드:{product.brand}</div>
-      <div>원산지:{product.country}</div>
-      <div>재고:{product.stock}</div>
-      <div>{product.description}</div>
-    </div>
+    !isLoading && (
+      <div className="page">
+        <img src={url} alt="사진" onError={handleImgError} />
+        <div>제품번호:{product.id}</div>
+        <div>카테고리:{product.category}</div>
+        <div>제품명:{product.name}</div>
+        <div>가격:{product.price}</div>
+        <div>브랜드:{product.brand}</div>
+        <div>원산지:{product.country}</div>
+        <div>재고:{product.stock}</div>
+        <div>판매자:{product.seller}</div>
+        <div>{product.description}</div>
+      </div>
+    )
   );
 }
 
